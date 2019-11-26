47711f23c93281221ec9e7fc5ef6e960e6df64fb
Dusanka Karac <karacdusanka11@gmail.com>
2019-11-26 20:27:07 +0100

    Drugi zadatak

--- src/components/AppCars.vue
+++ src/components/AppCars.vue
@@ -1,11 +1,29 @@
 <template>
     <div class="container">
         <h1>App Cars</h1>
+            <ul v-for="(car, key) in cars" :key="key">
+                {{ car.brand }}
+                {{ car.model }}
+            </ul> 
     </div>
 </template>
 
 <script>
+import { cars } from '../services/cars-service'
+
+
 export default {
+    data() {
+        return {
+            cars: null
+        }
+    },
+
+    created () {
+        cars.getAll().then(response =>{
+            this.cars = response.data
+      })
+    }
 
 }
 </script>
